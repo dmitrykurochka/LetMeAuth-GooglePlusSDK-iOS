f7c1c67ea81f0d26ed61682e0859e81bad64d8b7
Dmitry Kurochka <dimak@MacBook-Pro-Dmitry.local>
2015-07-30 23:43:22 +0300

    changed import

--- Pod/LMAGooglePlusSDKProvider.m
+++ Pod/LMAGooglePlusSDKProvider.m
@@ -8,8 +8,8 @@
 
 
 #import "LMAGooglePlusSDKProvider.h"
-#import <googleplus-ios-sdk/GooglePlus.h>
-#import <googleplus-ios-sdk/GTMOAuth2Authentication.h>
+#import <google-plus-ios-sdk/GooglePlus.h>
+#import <google-plus-ios-sdk/GTMOAuth2Authentication.h>
 
 
 @interface LMAGooglePlusSDKProvider () <GPPSignInDelegate>
